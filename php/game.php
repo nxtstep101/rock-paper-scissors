a48a53945a6772d596a0a628969b07508c934f0b
nxtstep101 <nxtstep101@gmail.com>
2014-05-20 21:55:28 -0500

    Update game.php

--- php/game.php
+++ php/game.php
@@ -7,16 +7,51 @@ $random = rand(1, 3);
 $cpu    = $random;
 $score  = 0;
 
-function rock() {
-//rock
+if (isset($_REQUEST['rock'])) {
+    rock();
+} elseif (isset($_REQUEST['paper'])) {
+    paper();
+} else {
+    scissors();
 }
 
-function paper() {
-//paper
+function rock()
+{
+    if ($cpu === 1) {
+        echo "It's a tie!";
+    } elseif ($cpu === 2) {
+        echo "You lose!";
+        $score--;
+    } else {
+        echo "You win!";
+        $score++;
+    }
 }
 
-function scissors() {
-//scissors
+function paper()
+{
+    if ($cpu === 1) {
+        echo "You win!";
+        $score++;
+    } elseif ($cpu === 2) {
+        echo "It's a tie!";
+    } else {
+        echo "You lose!";
+        $score--;
+    }
+}
+
+function scissors()
+{
+    if ($cpu === 1) {
+        echo "You lose!";
+        $score--;
+    } elseif ($cpu === 2) {
+        echo "You win!";
+        $score++;
+    } else {
+        echo "It's a tie!";
+    }
 }
 ?>
 <!DOCTYPE html>

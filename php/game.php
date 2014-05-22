cec72b988599cdbe6c8a5413062990770b3f17e9
nxtstep101 <nxtstep101@gmail.com>
2014-05-22 10:36:30 -0500

    Update game.php
    The separate functions were pretty redundant.

--- php/game.php
+++ php/game.php
@@ -8,41 +8,26 @@ $cpu    = $random;
 $score  = 0;
 
 if (isset($_REQUEST['rock'])) {
-    rock();
+    if ($cpu === 1) {
+        echo "It's a tie!";
+    } elseif ($cpu === 2) {
+        echo "You lose!";
+        $score--;
+    } else {
+        echo "You win!";
+        $score++;
+    }
 } elseif (isset($_REQUEST['paper'])) {
-    paper();
+    if ($cpu === 1) {
+        echo "It's a tie!";
+    } elseif ($cpu === 2) {
+        echo "You lose!";
+        $score--;
+    } else {
+        echo "You win!";
+        $score++;
+    }
 } else {
-    scissors();
-}
-
-function rock()
-{
-    if ($cpu === 1) {
-        echo "It's a tie!";
-    } elseif ($cpu === 2) {
-        echo "You lose!";
-        $score--;
-    } else {
-        echo "You win!";
-        $score++;
-    }
-}
-
-function paper()
-{
-    if ($cpu === 1) {
-        echo "You win!";
-        $score++;
-    } elseif ($cpu === 2) {
-        echo "It's a tie!";
-    } else {
-        echo "You lose!";
-        $score--;
-    }
-}
-
-function scissors()
-{
     if ($cpu === 1) {
         echo "You lose!";
         $score--;

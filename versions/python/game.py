a02a6eaabe640b09552c2533f96e5e3d7e237324
nxtstep101 <nxtstep101@gmail.com>
2014-05-25 23:33:20 -0500

    Creating function and fixing a few errors

--- versions/python/game.py
+++ versions/python/game.py
@@ -1,25 +1,30 @@
 from random import randrange
-user = raw_input('Rock, Paper, or Scissors?')
-reply = user.lower()
-cpu = randrange(2)
-if reply == 'rock':
-    if cpu == 0:
-        print "It's a tie"
-    elif cpu == 1:
-        print 'You win'
+def game():
+    user = raw_input('Rock, Paper, or Scissors?')
+    reply = user.lower()
+    cpu = randrange(2)
+    if reply == 'rock':
+        if cpu == 0:
+            print 'It\'s a tie'
+        elif cpu == 1:
+            print 'You win'
+        else:
+            print 'You lose'
+    elif reply == 'paper':
+        if cpu == 0:
+            print 'You win'
+        elif cpu == 1:
+            print 'It\'s a tie'
+        else:
+            print 'You lose'
+    elif reply == 'scissors':
+        if cpu == 0:
+            print 'You lose'
+        elif cpu == 1:
+            print 'You win'
+        else:
+            print 'It\'s a tie'
     else:
-        print 'You lose'
-elif reply == 'paper':
-    if cpu == 0:
-        print 'You win'
-    elif cpu == 1:
-        print "It's a tie"
-    else:
-        print 'You lose'
-else:
-    if cpu == 0:
-        print 'You lose'
-    elif cpu == 1:
-        print 'You win'
-    else:
-        print "It's a tie"
+        print "Please type either Rock, Paper, or Scissors"
+
+game()

79ce2020831d9e83765422f44db74309fa1c66da
nxtstep101 <nxtstep101@gmail.com>
2014-05-26 11:11:00 -0500

    Making it so that you can play multiple times

--- versions/python/game.py
+++ versions/python/game.py
@@ -1,32 +1,40 @@
 from random import randrange
 
+play_again = True
 def game():
-    user = raw_input('Rock, Paper, or Scissors?')
-    reply = user.lower()
-    cpu = randrange(2)
-    if reply == 'rock':
-        if cpu == 0:
-            print 'It\'s a tie'
-        elif cpu == 1:
-            print 'You win'
+    while play_again:
+        user = raw_input('Rock, Paper, or Scissors?')
+        reply = user.lower()
+        cpu = randrange(2)
+        if reply == 'rock':
+            if cpu == 0:
+                print 'It\'s a tie'
+            elif cpu == 1:
+                print 'You win'
+            else:
+                print 'You lose'
+        elif reply == 'paper':
+            if cpu == 0:
+                print 'You win'
+            elif cpu == 1:
+                print 'It\'s a tie'
+            else:
+                print 'You lose'
+        elif reply == 'scissors':
+            if cpu == 0:
+                print 'You lose'
+            elif cpu == 1:
+                print 'You win'
+            else:
+                print 'It\'s a tie'
         else:
-            print 'You lose'
-    elif reply == 'paper':
-        if cpu == 0:
-            print 'You win'
-        elif cpu == 1:
-            print 'It\'s a tie'
+            print 'Please type either Rock, Paper, or Scissors'
+        user_input = raw_input('Do you want to play again?')
+        replay = user_input.lower()
+        if replay == 'yes':
+            play_game = True
+        elif replay == 'no':
+            play_game = False
         else:
-            print 'You lose'
-    elif reply == 'scissors':
-        if cpu == 0:
-            print 'You lose'
-        elif cpu == 1:
-            print 'You win'
-        else:
-            print 'It\'s a tie'
-    else:
-        print 'Please type either Rock, Paper, or Scissors'
-
-
+            print 'Please only answer \"Yes\" or \"No\"'
 game()

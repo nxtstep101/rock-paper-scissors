5f3428a5966ca11e245a4558b80251621d85c88e
nxtstep101 <nxtstep101@gmail.com>
2014-05-27 23:34:31 -0500

    New scoring system and timeout
    New scoring system allows for more stats to be displayed; the timeout makes the game more realistic.

--- versions/javascript/game.js
+++ versions/javascript/game.js
@@ -1,65 +1,86 @@
 /***Rock, Paper, Scissors - by Nxtstep101***/
-//Rock = 1
+//Rock = 1 
 //Paper = 2 
 //Scissors = 3
-var score = 0;
+var wins = 0;
+var losses = 0;
+var ties = 0;
+var score = wins - losses;
 
 function rock() {
-    var cpu = Math.floor((Math.random() * 3) + 1);
-    if (cpu === 1) {
-        $('#cpu').text("Rock");
-        $('#you').text("Rock");
-        $('#result').text("It's a tie!");
-    } else if (cpu === 2) {
-        $('#cpu').text("Paper");
-        $('#you').text("Rock");
-        $('#result').text("You lose!");
-        score -= 1;
-    } else {
-        $('#cpu').text("Scissors");
-        $('#you').text("Rock");
-        $('#result').text("You win!");
-        score += 1;
-    }
-    $("#score").text(score);
+    setTimeout(function() {
+        var cpu = Math.floor((Math.random() * 3) + 1);
+        if (cpu === 1) {
+            $('#cpu').text("Rock");
+            $('#you').text("Rock");
+            $('#result').text("It's a tie!");
+            ties += 1;
+        } else if (cpu === 2) {
+            $('#cpu').text("Paper");
+            $('#you').text("Rock");
+            $('#result').text("You lose!");
+            losses += 1;
+        } else {
+            $('#cpu').text("Scissors");
+            $('#you').text("Rock");
+            $('#result').text("You win!");
+            wins += 1;
+        }
+        $("#wins").text(wins);
+        $("#losses").text(losses);
+        $("#ties").text(ties);
+        $("#score").text(score);
+    }, 250);
 }
 
 function paper() {
-    var cpu = Math.floor((Math.random() * 3) + 1);
-    if (cpu === 1) {
-        $('#cpu').text("Rock");
-        $('#you').text("Paper");
-        $('#result').text("You win!");
-        score += 1;
-    } else if (cpu === 2) {
-        $('#cpu').text("Paper");
-        $('#you').text("Paper");
-        $('#result').text("It's a tie!");
-    } else {
-        $('#cpu').text("Scissors");
-        $('#you').text("Paper");
-        $('#result').text("You lose!");
-        score -= 1;
-    }
-    $("#score").text(score);
+    setTimeout(function() {
+        var cpu = Math.floor((Math.random() * 3) + 1);
+        if (cpu === 1) {
+            $('#cpu').text("Rock");
+            $('#you').text("Paper");
+            $('#result').text("You win!");
+            wins += 1;
+        } else if (cpu === 2) {
+            $('#cpu').text("Paper");
+            $('#you').text("Paper");
+            $('#result').text("It's a tie!");
+            ties += 1;
+        } else {
+            $('#cpu').text("Scissors");
+            $('#you').text("Paper");
+            $('#result').text("You lose!");
+            losses += 1;
+        }
+        $("#wins").text(wins);
+        $("#losses").text(losses);
+        $("#ties").text(ties);
+        $("#score").text(score);
+    }, 250);
 }
 
 function scissors() {
-    var cpu = Math.floor((Math.random() * 3) + 1);
-    if (cpu === 1) {
-        $('#cpu').text("Rock");
-        $('#you').text("Scissors");
-        $('#result').text("You lose!");
-        score -= 1;
-    } else if (cpu === 2) {
-        $('#cpu').text("Paper");
-        $('#you').text("Scissors");
-        $('#result').text("You win!");
-        score += 1;
-    } else {
-        $('#cpu').text("Scissors");
-        $('#you').text("Scissors");
-        $('#result').text("It's a tie!");
-    }
-    $("#score").text(score);
+    setTimeout(function() {
+        var cpu = Math.floor((Math.random() * 3) + 1);
+        if (cpu === 1) {
+            $('#cpu').text("Rock");
+            $('#you').text("Scissors");
+            $('#result').text("You lose!");
+            losses += 1;
+        } else if (cpu === 2) {
+            $('#cpu').text("Paper");
+            $('#you').text("Scissors");
+            $('#result').text("You win!");
+            wins += 1;
+        } else {
+            $('#cpu').text("Scissors");
+            $('#you').text("Scissors");
+            $('#result').text("It's a tie!");
+            ties += 1;
+        }
+        $("#wins").text(wins);
+        $("#losses").text(losses);
+        $("#ties").text(ties);
+        $("#score").text(score);
+    }, 250);
 }

ff9ae1ccc7f5c9ddecbf4a021a157225003c4dc8
nxtstep101 <nxtstep101@gmail.com>
2014-05-29 13:42:13 -0500

    Fixing the "play again" feature
    Also fixed the "quit" feature

--- versions/python/game.py
+++ versions/python/game.py
@@ -1,40 +1,49 @@
 from random import randrange
 
-play_again = True
 def game():
-    while play_again:
+    while True:
+        def again():
+            user_input = raw_input('Do you want to play again?')
+            play_again = user_input.lower()
+            if play_again == 'yes':
+                return True
+            elif play_again == 'no':
+                return False
+            else:
+                print 'Please only answer \"Yes\" or \"No\"'
         user = raw_input('Rock, Paper, or Scissors?')
         reply = user.lower()
         cpu = randrange(2)
         if reply == 'rock':
             if cpu == 0:
                 print 'It\'s a tie'
+                again()
             elif cpu == 1:
                 print 'You win'
+                again()
             else:
                 print 'You lose'
+                again()
         elif reply == 'paper':
             if cpu == 0:
                 print 'You win'
+                again()
             elif cpu == 1:
                 print 'It\'s a tie'
+                again()
             else:
                 print 'You lose'
+                again()
         elif reply == 'scissors':
             if cpu == 0:
                 print 'You lose'
+                again()
             elif cpu == 1:
                 print 'You win'
+                again()
             else:
                 print 'It\'s a tie'
+                again()
         else:
             print 'Please type either Rock, Paper, or Scissors'
-        user_input = raw_input('Do you want to play again?')
-        replay = user_input.lower()
-        if replay == 'yes':
-            play_game = True
-        elif replay == 'no':
-            play_game = False
-        else:
-            print 'Please only answer \"Yes\" or \"No\"'
 game()

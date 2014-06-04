857ed59232ca48776a6b9c35926dafae42772b24
nxtstep101 <nxtstep101@gmail.com>
2014-06-03 22:33:01 -0500

    Adding game function and minor formatting
    Use the start game function to create a fancy start page

--- versions/php/game.php
+++ versions/php/game.php
@@ -3,53 +3,58 @@
 //Rock = 1
 //Paper = 2 
 //Scissors = 3
-session_start();
-$random = rand(1, 3);
-$cpu    = $random;
-$win    = "Result: You win!";
-$loss   = "Result: You lose!";
-$tie    = "Result: It's a tie!";
-$_SESSION['score'] = ((isset($_SESSION['score'])) ? $_SESSION['score'] : 0);
-
-if (isset($_REQUEST['rock'])) {
-    if ($cpu === 1) {
-        echo "CPU: Rock<br/>" . $tie;
-    } elseif ($cpu === 2) {
-        echo "CPU: Paper<br/>" . $loss;
-        $_SESSION['score']--;
+function startGame()
+{
+    session_start();
+    $random            = rand(1, 3);
+    $cpu               = $random;
+    $win               = "Result: You win!";
+    $loss              = "Result: You lose!";
+    $tie               = "Result: It's a tie!";
+    $_SESSION['score'] = ((isset($_SESSION['score'])) ? $_SESSION['score'] : 0);
+    
+    if (isset($_REQUEST['rock'])) {
+        if ($cpu === 1) {
+            echo "CPU: Rock<br/>" . $tie;
+        } elseif ($cpu === 2) {
+            echo "CPU: Paper<br/>" . $loss;
+            $_SESSION['score']--;
+        } else {
+            echo "CPU: Scissors<br/>" . $win;
+            $_SESSION['score']++;
+        }
+    } elseif (isset($_REQUEST['paper'])) {
+        if ($cpu === 1) {
+            echo "CPU: Rock<br/>" . $win;
+        } elseif ($cpu === 2) {
+            echo "CPU: Paper<br/>" . $tie;
+            $_SESSION['score']++;
+        } else {
+            echo "CPU: Scissors<br/>" . $loss;
+            $_SESSION['score']--;
+        }
+    } elseif (isset($_REQUEST['scissors'])) {
+        if ($cpu === 1) {
+            echo "CPU: Rock<br/>" . $loss;
+            $_SESSION['score']--;
+        } elseif ($cpu === 2) {
+            echo "CPU: Paper<br/>" . $win;
+            $_SESSION['score']++;
+        } else {
+            echo "CPU: Scissors<br/>" . $tie;
+        }
     } else {
-        echo "CPU: Scissors<br/>" . $win;
-        $_SESSION['score']++;
+        session_unset();
     }
-} elseif (isset($_REQUEST['paper'])) {
-    if ($cpu === 1) {
-        echo "CPU: Rock<br/>" . $win;
-    } elseif ($cpu === 2) {
-        echo "CPU: Paper<br/>" . $tie;
-        $_SESSION['score']++;
-    } else {
-        echo "CPU: Scissors<br/>" . $loss;
-        $_SESSION['score']--;
-    }
-} elseif (isset($_REQUEST['scissors'])) {
-    if ($cpu === 1) {
-        echo "CPU: Rock<br/>" . $loss;
-        $_SESSION['score']--;
-    } elseif ($cpu === 2) {
-        echo "CPU: Paper<br/>" . $win;
-        $_SESSION['score']++;
-    } else {
-        echo "CPU: Scissors<br/>" . $tie;
-    }
-} else {
-    session_unset();
+    echo "<br />Score: " . $_SESSION['score'];
 }
-echo "<br />Score: " . $_SESSION['score'];
+
+startGame();
 ?>
 <!DOCTYPE html>
 <html>
   <body>
-    <form action="game.php" method="post">
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
       <input type="submit" name="rock" value="rock">
       <input type="submit" name="paper" value="paper">
       <input type="submit" name="scissors" value="scissors">

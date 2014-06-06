0f6f9848e9068f2658eac613705c05da00fc8fee
nxtstep101 <nxtstep101@gmail.com>
2014-06-05 23:52:07 -0500

    Update start.php

--- versions/php/start.php
+++ versions/php/start.php
@@ -3,7 +3,6 @@ function start_popup()
 {
     echo "<div id='start_popup'>Rock, Paper, Scissors!<br />By - Nxtstep101<br /><input type='submit' name='start_game' value='Start game'>";
     if ($_POST['start_game']) {
-        exit();
         startGame();
     }
 }

e84becb5d96e082ef5e73562bfdcd0549fa99e35
nxtstep101 <nxtstep101@gmail.com>
2014-06-13 19:57:18 -0500

    Reminder - fix play again feature

--- versions/python/game.py
+++ versions/python/game.py
@@ -2,15 +2,6 @@ from random import randrange
 
 def game():
     while True:
-        def again():
-            user_input = raw_input('Do you want to play again?')
-            play_again = user_input.lower()
-            if play_again == 'yes':
-                return True
-            elif play_again == 'no':
-                return False
-            else:
-                print 'Please only answer \"Yes\" or \"No\"'
         user = raw_input('Rock, Paper, or Scissors?')
         reply = user.lower()
         cpu = randrange(2)

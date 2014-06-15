6ddf3cc28b0c785b36bf78f35ee568a1f7a8457e
nxtstep101 <nxtstep101@gmail.com>
2014-06-15 15:07:41 -0500

    simplified

--- versions/python/game.py
+++ versions/python/game.py
@@ -1,40 +1,17 @@
 from random import randrange
 
-def game():
-    while True:
-        user = raw_input('Rock, Paper, or Scissors?')
-        reply = user.lower()
-        cpu = randrange(2)
-        if reply == 'rock':
-            if cpu == 0:
-                print 'It\'s a tie'
-                again()
-            elif cpu == 1:
-                print 'You win'
-                again()
-            else:
-                print 'You lose'
-                again()
-        elif reply == 'paper':
-            if cpu == 0:
-                print 'You win'
-                again()
-            elif cpu == 1:
-                print 'It\'s a tie'
-                again()
-            else:
-                print 'You lose'
-                again()
-        elif reply == 'scissors':
-            if cpu == 0:
-                print 'You lose'
-                again()
-            elif cpu == 1:
-                print 'You win'
-                again()
-            else:
-                print 'It\'s a tie'
-                again()
-        else:
-            print 'Please type either Rock, Paper, or Scissors'
-game()
+while True:
+    user_reply = raw_input('Rock, Paper, or Scissors?')
+    reply = user_reply.lower()
+    cpu = randrange(3)
+    if reply == 'rock' and cpu == 0 or reply == 'paper' and cpu == 1 or reply == 'scissors' and cpu == 2:
+        print 'Its a tie'
+        print cpu, reply
+    elif reply == 'rock' and cpu == 1 or reply == 'paper' and cpu == 0 or reply == 'scissors' and cpu == 0:
+        print 'You lose'
+        print cpu, reply
+    elif reply == 'rock' and cpu == 2 or reply == 'paper' and cpu == 0 or reply == 'scissors' and cpu == 1:
+        print 'You win'
+        print cpu, reply
+	else:
+		print "Please type either rock, paper, or scissors"
